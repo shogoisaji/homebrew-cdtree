59d665f3f0eb18f9840f71ddc2e5efad97025646
shogoisaji <simacsimac9@gmail.com>
2026-02-09 00:14:08 +0900

    Update SHA256 for v0.1.4

--- cdtree.rb
+++ cdtree.rb
@@ -2,7 +2,7 @@ class Cdtree < Formula
   desc "A dynamic tree-based directory navigation tool"
   homepage "https://github.com/shogoisaji/cdtree"
   url "https://github.com/shogoisaji/cdtree/releases/download/v0.1.4/cdtree-mac.tar.gz"
-  sha256 "ff83a4abbe89a56243b82c6b1d8b0ac55e700f85e7a5e3fe064bc8b466de2bed"
+  sha256 "5051833886e6e077dad96f7185620d2d619e934cd7d05508ed4430ebe91e4054"
   version "0.1.4"
 
   def install
