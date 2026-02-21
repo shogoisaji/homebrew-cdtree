ce485a606ba80c9fb2091e6496d8835b30a163d0
shogoisaji <simacsimac9@gmail.com>
2026-02-21 16:54:37 +0900

    Update cdtree to 0.1.6

--- cdtree.rb
+++ cdtree.rb
@@ -1,9 +1,9 @@
 class Cdtree < Formula
   desc "A dynamic tree-based directory navigation tool"
   homepage "https://github.com/shogoisaji/cdtree"
-  url "https://github.com/shogoisaji/cdtree/releases/download/v0.1.5/cdtree-mac.tar.gz"
-  sha256 "8cb6e748c1a517415dafb883fd1ac6afe349f26e08fe92d2b6b56bfba9af1e41"
-  version "0.1.5"
+  url "https://github.com/shogoisaji/cdtree/releases/download/v0.1.6/cdtree-mac.tar.gz"
+  sha256 "8126ff6866fa31982ef0f5a2bb7918fa855d8a9a93c6ba3cc4020710e12453cb"
+  version "0.1.6"
 
   def install
     bin.install "cdtree"
